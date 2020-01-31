8b79aa6c6476b5dbdbbde056d4d86f0623febff1
Ivana <ivana.huckova@gmail.com>
2020-01-31 09:34:51 +0100

    Update

--- src/Components/Akcie.js
+++ src/Components/Akcie.js
@@ -17,14 +17,18 @@ import neonka from '../assets/logo_image.svg';
 export default class Akcie extends React.Component {
     render() {
         return (
-            <StyledMidBanner id='mikulas'>
-                <h1>Mikuláš sa vrátil do DFN</h1>
+            <StyledMidBanner id='novinky'>
+                <h1>Novinky</h1>
+                <h2>Každý hlas v ankete Mobil roka 2019 pomôže Neónke</h2>
+                <div>Každý hlas v ankete Mobil roka 2019 vhodí do našej pomyselnej pokladničky päť centov. Hlasujte <a href="https://techbox.dennikn.sk/temy/kazdy-hlas-v-ankete-mobil-roka-2019-powered-by-o2-pomoze-detom-v-nemocnici/?fbclid=IwAR0n8IXNq4rmGrxaNsZt4BI8FHmsg5vJjBACU4QobErb1VVRY9sS1RQCnvI">tu</a>.</div>
+                <div className='logo'><img alt="logo neonky" src={neonka} /></div>
+                <h2 id='mikulas'>Mikuláš sa vrátil do DFN</h2>
                 <div>6. ročník Charitatívneho Mikulášskeho behu dopadol nad naše očakávanie, s rekordnou účasťou 222 mikulášov. Všetci sme behali za spoločný cieľ, za deti s cystickou fibrózou.</div>
                 <div>Za vyzbierané peniaze sa nám podarilo zakúpiť 5 najmodernejších inhalátorov od spoločnosti MR diagno - stic. Prístroje budú venované 5 deťom s cystickou fibrózou. Tieto deti boli zachytené novorodeneckým skríningom a sú mladšie ako dva roky. Inhalátory im zlepšia kvalitu života - pocit nadýchnuť sa!</div>
                 <div>Oficiálne odovzdanie prístrojov rodinám bude za účasti Dr. Takáča, ktorý vedie Centrum pre liečbu cystickej fibrózy v Banskej Bystrici. Prístroje odovzdajú organizátori behu - OZ Neónka Banská Bystrica a OZ Crazy woman v zastúpení Dr. Belicovej.</div>
                 <div className="bold">Save the date! 12.2.2020, o 16:00 v zasadačke DFN v Banskej Bystrici.</div>
                 <div>Občerstvenie zabezpečíme.</div>
-                <div>Vaše Neónky!<img alt="logo neonky" src={neonka}></img></div>
+                <div className='logo'>Vaše Neónky!<img alt="logo neonky" src={neonka}></img></div>
             </StyledMidBanner>
         );
     }
@@ -38,6 +42,7 @@ const StyledMidBanner = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
 
     @media screen and (max-width: 1500px) {
         padding: 10vh 15vw;
@@ -48,11 +53,28 @@ const StyledMidBanner = styled.div`
     .bold {
         font-weight: bold;
     }
+    .logo {
+        width: 94px;
+    }
+    a {
+        color: ${colors.pinkColor};
+        font-weight: bold;
+    }
+    img {
+        width: 100%;
+    }
     h1 {
         font-size: ${fonts.mainHeadingScreen};
         text-align: center;
         @media screen and (max-width: 600px) {
             font-size: ${fonts.mainHeadingMobile};
+        }
+    h2 {
+        margin-top: 0;
+        font-size: ${fonts.subHeadingScreen};
+        text-align: center;
+        @media screen and (max-width: 600px) {
+            font-size: ${fonts.subHeadingMobile};
         }
     }
     .banner {
