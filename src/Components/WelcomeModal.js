d85db7134d36f1804ba3deee0e58e3b243c1b820
ivanahuckova <ivana.huckova@gmail.com>
2025-08-06 21:46:21 +0200

    Fix model

--- src/Components/WelcomeModal.js
+++ src/Components/WelcomeModal.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import Modal from "react-awesome-modal";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import Modal from 'react-awesome-modal';
+import styled from 'styled-components';
 
 //Colors + Fonts
-import * as colors from "../style/colors";
+import * as colors from '../style/colors';
 
 import qrPodpora from '../assets/qr_neonka_akvarium.png';
 
@@ -13,34 +13,28 @@ export default class WelcomeModal extends Component {
   };
   render() {
     return (
-      <Modal
-        visible={this.props.visible}
-        effect="fadeInUp"
-        onClickAway={() => this.props.closeModal()}
-      >
+      <Modal visible={this.props.visible} effect="fadeInUp" onClickAway={this.props.closeModal}>
         <StyledModal>
-          <h2 style={{ margin: "10px", textAlign: "center" }}>
-            Zbierka „Akvárium“ pre detskú pľúcnu ambulanciu
-          </h2>
+          <h2 style={{ margin: '10px', textAlign: 'center' }}>Zbierka „Akvárium“ pre detskú pľúcnu ambulanciu</h2>
           <div style={{ textAlign: 'center', margin: '0 0 16px 0' }}>
             Pomôžte nám dofinancovať prístroj na presnú diagnostiku detských pľúcnych ochorení.
           </div>
-          <img src={qrPodpora} alt="QR kód pre podporu" style={{ maxWidth: 180, margin: '8px auto', display: 'block' }} />
-          <div style={{ textAlign: 'center', fontWeight: 'bold', margin: '8px 0' }}>
-            SK72 0900 0000 0052 2909 7405
-          </div>
+          <img
+            src={qrPodpora}
+            alt="QR kód pre podporu"
+            style={{ maxWidth: 180, margin: '8px auto', display: 'block' }}
+          />
+          <div style={{ textAlign: 'center', fontWeight: 'bold', margin: '8px 0' }}>SK72 0900 0000 0052 2909 7405</div>
           <StyledButtons>
             <span
               onClick={() => {
                 this.props.closeModal();
-                window.location.replace("/akvarium");
+                window.location.replace('/akvarium');
               }}
             >
               Chcem sa dozvedieť viac
             </span>
-            <span onClick={() => this.props.closeModal()}>
-              Pokračovať na stránku
-            </span>
+            <span onClick={this.props.closeModal}>Pokračovať na stránku</span>
           </StyledButtons>
         </StyledModal>
       </Modal>
